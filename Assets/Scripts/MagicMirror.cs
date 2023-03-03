8c3183e78c490301a9ad37312540444f46bc420b
deancohen1234 <dean3712@gmail.com>
2023-03-03 11:10:09 -0700

    Added in music and more sounds

--- Assets/Scripts/MagicMirror.cs
+++ Assets/Scripts/MagicMirror.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using DG.Tweening;
 
 public class MagicMirror : Location
 {
@@ -8,6 +10,7 @@ public class MagicMirror : Location
 
     public Material omenMaterial;
     public AnimationCurve alphaCurve;
+    public AudioMixer audioMixer;
 
     public Vector2 minMaxWaitDuration = new Vector2(0.5f, 2f);
     public Vector2 minMaxHoldDuration = new Vector2(1.5f, 4.0f);
@@ -16,11 +19,25 @@ public class MagicMirror : Location
     public float omenTorqueSpeed = 5f;
     public float omenGravity = 0.1f;
 
+    public float volumeFadeDuration = 0.75f;
+
     private GameObject spawnedOmen;
     private Rigidbody omenRB;
+    private AudioSource audioSource;
 
     private float omenDespawnTime;
     private float omenSpawnTime;
+
+    private const float LOW_PASS_THRESHOLD_DEFAULT = 5000;
+    private const float LOW_PASS_THRESHOLD_ON = 350;
+
+    private void Start() {
+        audioSource = GetComponent<AudioSource>();
+
+        audioSource.volume = 0;
+
+        audioMixer.DOSetFloat("LowPassCutoff", LOW_PASS_THRESHOLD_DEFAULT, volumeFadeDuration);
+    }
 
     // Update is called once per frame
     void Update()
@@ -60,6 +77,8 @@ public class MagicMirror : Location
         base.Enter();
 
         omenSpawnTime = Time.time + Random.Range(minMaxWaitDuration.x, minMaxWaitDuration.y);
+
+        StartAudio();
     }
 
     public override void Exit() {
@@ -67,6 +86,8 @@ public class MagicMirror : Location
         DespawnOmen();
 
         base.Exit();
+
+        StopAudio();
     }
 
     private void SpawnOmen() {
@@ -110,5 +131,22 @@ public class MagicMirror : Location
 
         Vector3 directionToCenter = transform.position - omenRB.position;
         omenRB.AddForce(directionToCenter.normalized * directionToCenter.magnitude * directionToCenter.magnitude * omenGravity, ForceMode.Acceleration);
+    }
+
+    private void StartAudio() {
+        //audioSource.Play();
+
+        audioMixer.DOSetFloat("LowPassCutoff", LOW_PASS_THRESHOLD_ON, volumeFadeDuration);
+
+        audioSource.DOFade(1.0f, volumeFadeDuration);
+
+    }
+
+    private void StopAudio() {
+
+        //audioSource.Stop();
+        audioMixer.DOSetFloat("LowPassCutoff", LOW_PASS_THRESHOLD_DEFAULT, volumeFadeDuration);
+
+        audioSource.DOFade(0, volumeFadeDuration);
     }
 }
